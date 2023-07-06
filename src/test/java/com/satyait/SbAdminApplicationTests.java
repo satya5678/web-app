9d85aa64116b0933dade929d3d68c6a9aca92eb3
Satyakumar <satishsatish6955@gmail.com>
2023-07-06 22:59:50 +0530

    initial commit

---
+++ src/test/java/com/satyait/SbAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
